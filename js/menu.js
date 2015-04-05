1b9f2c3ebacb25d094e95f8d61b6425628792602
Juan David Hermoso <juandavidhermoso@gmail.com>
2015-04-05 19:43:00 +0200

    Add demo link in readme.txt

--- js/menu.js
+++ js/menu.js
@@ -8,11 +8,12 @@
 	var toggleButton = document.getElementsByClassName('responsive-menu-button')[0];
 
 	var toggle = function(elem) {
+
+		//This function receives an element to show or hide according to it's state.
+
  		var element = elem;
 
- 		var display = window.getComputedStyle(navbar).display;
-
- 		console.log(display);
+ 		var display = window.getComputedStyle(navbar).display; 		
 
  		if ( display === 'none') {
 
--- readme.txt
+++ readme.txt
@@ -1,2 +1,6 @@
 /** RESPONSIVE NAVBAR HTML5 + CSS3 + JS (for responsiveness) **/
 
+
+DEMO HERE:
+
+http://juandavidhermoso.es/html5navbar
